37a05ea25f57865f41edd83cccffc6adc9f60846
petersondelsi <petersonoliveiradelsi@gmail.com>
2025-02-16 20:47:33 +0000

    responsive

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function showSlides(n) {
+    let i;  
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    
+    if (n > slides.length) 
+        {slideIndex = 1;}    
+    if (n < 1) 
+        {slideIndex = slides.length}
+
+    for (i = 0; i < slides.length; i++) {
+        console.log(slides[i].style.display);
+        slides[i].style.display = "none";  
+        console.log(i);
+        
+    }
+
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
